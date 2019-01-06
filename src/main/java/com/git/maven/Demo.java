919253abc84f5502efda7f7b24591a3ba106dd45
bj <bj@163.com>
2019-01-06 21:34:55 +0800

    first commit local rep

---
+++ src/main/java/com/git/maven/Demo.java
@@ -0,0 +1,14 @@
+package com.git.maven;
+
+/**
+ * ClassName:Demo
+ * package:com.git.maven
+ * Description:
+ * Date:2019/1/6 21:25
+ * Author:Qjy
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
